d69d3b42ec5ad4f6a755cb7cffc9cbc2259fbb25
MirzaSafunur <mirzasafunur017@gmail.com>
2024-10-20 13:08:02 +0600

    first commit

---
+++ Untitled-1.js
@@ -0,0 +1,10 @@
+let trainees = ["Rajib", "Ali", "Rafi", "Rabbni"];
+document.write(trainees[2])
+console.log(trainees.join("*"))
+console.log(trainees.concat("Shamony", "Rukiya"));
+
+console.log(trainees.toString());
+
+let letter = ["B", "D""C", "A""E", "G", "F"];
+console.log(letter.sort());
+console.log(letter.reverse());
